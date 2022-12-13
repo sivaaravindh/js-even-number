5647ab907caecef22999900b9e785c6de1cd20ed
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-13 08:24:38 -0800

    index

---
+++ main.js
@@ -0,0 +1,26 @@
+let a=parseInt (prompt("Enter the value"));
+
+for (var x=0; x<=a; x++) {
+    if (x === 0) {
+            console.log(x +  " is even");
+    }
+    else{
+         if (x % 2 === 0) {
+            console.log(x + " is even");   
+    }
+}
+    
+}
+ 
+/*for (var x=0; x<=15; x++) {
+        if (x === 0) {
+                console.log(x +  " is even");
+        }
+        else if (x % 2 === 0) {
+                console.log(x + " is even");   
+        }
+        else {
+                console.log(x + " is odd");
+        }
+    }
+    */
